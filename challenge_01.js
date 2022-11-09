d0c6b1ec6ff329612db625bef3094b125e1f9fab
Mia Kamenetski <mia.kamenetski@online.de>
2022-11-09 11:57:00 +0100

    Variante 1

--- challenge_01.js
+++ challenge_01.js
@@ -10,12 +10,25 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 
 /*********  Variante 1 **********/
 /*
+
+//Deklaration
+let ziffer1, ziffer2
 let zahl1, zahl2;
 let summe;
 
-zahl1 = prompt("Zahl 1:");
-zahl2 = prompt("Zahl 2:");
+//Eingabe
+ziffer1 = prompt("Zahl 1:");
+ziffer2 = prompt("Zahl 2:");
+console.log(typeof zíffer1);
+console.log(typeof ziffer2);
 
+//Typkonvertierung
+zahl1 = parseInt(ziffer1);
+zahl2 = parseInt(ziffer2);
+console.log(typeof zahl1);
+console.log(typeof zahl2);
+
+//Berechnung
 summe = zahl1 + zahl2;
 
 console.log("Die Summe der Zahlen ist: " + summe);
@@ -23,13 +36,21 @@ console.log("Die Summe der Zahlen ist: " + summe);
 
 //Deklaration
 let zahl1, zahl2;
+let summe;
 
 //Wertzuweisung
-zahl1 = ("Bitte eine Zahl eingeben:");
-zahl2 = ("Bitte eine zweite Zahl eingeben:");
+zahl1 = Number(prompt("Bitte eine Zahl eingeben:"));
+zahl2 = Number(prompt("Bitte eine zweite Zahl eingeben:"));
 
 //Berechnung
-summe = Number(prompt(zahl1)) + Number(prompt(zahl2))
+summe = zahl1 + zahl2
 
 //Ausgabe
-console.log("Die Summe der Zahlen ist: " + summe)
+// console.log("Die Summe der Zahlen ist: " + summe)
+function sumation(zahl1,zahl2){
+    if (typeof zahl1 == Number & typeof zahl2 == Number)
+    return ("Die Summe der Zahlen ist: " + summe)
+    else
+    alert ("Falscher Eingabewert");
+}
+
