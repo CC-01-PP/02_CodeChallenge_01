6789b1ad59c527e245607629e286fd9c9289df02
Mia Kamenetski <mia.kamenetski@online.de>
2022-11-09 12:21:51 +0100

    Variante 2

--- challenge_01.js
+++ challenge_01.js
@@ -9,48 +9,59 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 
 /*********  Variante 1 **********/
-/*
 
-//Deklaration
-let ziffer1, ziffer2
-let zahl1, zahl2;
-let summe;
+// //Deklaration
+// let ziffer1, ziffer2
+// let zahl1, zahl2;
+// let summe;
 
-//Eingabe
-ziffer1 = prompt("Zahl 1:");
-ziffer2 = prompt("Zahl 2:");
-console.log(typeof zíffer1);
-console.log(typeof ziffer2);
+// //Eingabe
+// ziffer1 = prompt("Zahl 1:");
+// ziffer2 = prompt("Zahl 2:");
+// console.log(typeof ziffer1);
+// console.log(typeof ziffer2);
 
-//Typkonvertierung
-zahl1 = parseInt(ziffer1);
-zahl2 = parseInt(ziffer2);
-console.log(typeof zahl1);
-console.log(typeof zahl2);
+// //Typkonvertierung
+// zahl1 = parseInt(ziffer1);
+// zahl2 = parseInt(ziffer2);
+// console.log(typeof zahl1);
+// console.log(typeof zahl2);
 
-//Berechnung
-summe = zahl1 + zahl2;
+// //Berechnung
+// summe = zahl1 + zahl2;
 
-console.log("Die Summe der Zahlen ist: " + summe);
-*/
+// console.log("Die Summe der Zahlen ist: " + summe);
 
-//Deklaration
-let zahl1, zahl2;
-let summe;
+/*********  Variante 2 **********/
 
-//Wertzuweisung
-zahl1 = Number(prompt("Bitte eine Zahl eingeben:"));
-zahl2 = Number(prompt("Bitte eine zweite Zahl eingeben:"));
+//kompaktere Lösung
 
-//Berechnung
+let zahl1, zahl2, summe;
+
+zahl1 = parseInt(prompt("Zahl 1:"));
+zahl2 = parseInt(prompt("Zahl 2: "));
 summe = zahl1 + zahl2
+console.log("Die Summe der Zahlen ist: " + summe);
 
-//Ausgabe
+/*********  Variante 0 **********/
+
+// //Deklaration
+// let zahl1, zahl2;
+// let summe;
+
+// //Wertzuweisung
+// zahl1 = Number(prompt("Bitte eine Zahl eingeben:"));
+// zahl2 = Number(prompt("Bitte eine zweite Zahl eingeben:"));
+
+// //Berechnung
+// summe = zahl1 + zahl2
+
+// //Ausgabe
 // console.log("Die Summe der Zahlen ist: " + summe)
-function sumation(zahl1,zahl2){
-    if (typeof zahl1 == Number & typeof zahl2 == Number)
-    return ("Die Summe der Zahlen ist: " + summe)
-    else
-    alert ("Falscher Eingabewert");
-}
+// // function sumation(zahl1,zahl2){
+// //     if (typeof zahl1 == Number & typeof zahl2 == Number)
+// //     return ("Die Summe der Zahlen ist: " + summe)
+// //     else
+// //     alert ("Falscher Eingabewert");
+// // }
 
